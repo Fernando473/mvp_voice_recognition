f96f99c18190e8ff5cfd2ab22067ff26f3b2f21f
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-01-30 13:24:13 -0500

    Refactor(Handling exceptions during each case)

--- speech_to_text/views.py
+++ speech_to_text/views.py
@@ -1,3 +1,7 @@
+import os
+import tempfile
+
+from django.core.exceptions import SuspiciousOperation
 from django.shortcuts import render
 from rest_framework.views import APIView
 
@@ -45,42 +49,34 @@ from django.http import JsonResponse
 
 
 def upload_audio(request):
-    if request.method == 'POST':
+    try:
         audio_file = request.FILES.get('audio_file')
 
-        if audio_file:
-            try:
-                # Guarda el archivo localmente
-                audio_file_path = f"audio_temp/{audio_file.name}"
-                with open(audio_file_path, 'wb') as audio_file_local:
-                    for chunk in audio_file.chunks():
-                        audio_file_local.write(chunk)
+        if not audio_file:
+            raise SuspiciousOperation('Archivo de audio no recibido en la solicitud')
 
-                # Transcribe el audio con Whisper
-                wc = WhisperClient()
+        # Crear un archivo temporal para el audio
+        with tempfile.NamedTemporaryFile(delete=False) as temp_audio_file:
+            for chunk in audio_file.chunks():
+                temp_audio_file.write(chunk)
 
-                # Realiza la transcripción y devuelve el texto
-                text = wc.transcribe_audio(audio_file_path)
-
-                return JsonResponse({'success': True, 'transcription': text})
-            except Exception as e:
-                print(f"Error en la transcripción: {e}")
-
-                return JsonResponse({'success': False, 'error': str(e)})
-        else:
+        try:
             wc = WhisperClient()
+            text = wc.transcribe_audio(temp_audio_file.name)
+            return JsonResponse({'success': True, 'transcription': text})
+        except Exception as e:
+            raise SuspiciousOperation(f'Error en la transcripción: {e}')
 
-            # Realiza la transcripción y devuelve el texto
-            text = wc.transcribe_audio("audio_2023062")
-            print("Texto generad0,", text)
-            return JsonResponse({'success': False, 'error': 'Archivo de audio no recibido en la solicitud'})
-    else:
-        wc = WhisperClient()
+    except SuspiciousOperation as e:
+        return JsonResponse({'success': False, 'error': str(e)})
 
-        # Realiza la transcripción y devuelve el texto
-        text = wc.transcribe_audio("audio_2023062")
-        print("Texto generad0,", text)
-        return JsonResponse({'success': False, 'error': 'Método de solicitud no válido'})
+    except Exception as e:
+        # Loguear otros errores no manejados
+        print(f"Error no manejado: {e}")
+        return JsonResponse({'success': False, 'error': 'Error interno del servidor'})
+    finally:
+        # Eliminar el archivo temporal después de usarlo
+        os.remove(temp_audio_file.name)
 
 
 def upload_audio_page(request):
